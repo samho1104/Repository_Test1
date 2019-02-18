78d14de856382b62bc0d3ede00673f73cfb8030e
Ting-Yu Chao <raspruino@gmail.com>
2019-02-18 22:33:31 +0800

    加入favico

--- hello.py
+++ hello.py
@@ -3,17 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '''
-    <html>
-      <head>
-        <meta charset="utf-8">
-        <title>世說鮮語</title>
-      </head>
-      <body>
-        人家有的是背景，<br>而我有的只是背影…
-      </body>
-    </html>
-    '''
+    return app.send_static_file('index.html')
     
 if __name__ == "__main__":
     app.run('0.0.0.0', 80)
