b8e162b5a016c6be3a804c75ab6f2daacf000c96
Ting-Yu Chao <raspruino@gmail.com>
2019-02-18 22:41:42 +0800

    簡化HTML

--- hello.py
+++ hello.py
@@ -3,7 +3,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return '''
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>世說鮮語</title>
+      </head>
+      <body>
+        人家有的是背景，而我有的只是背影…
+      </body>
+    </html>
+    '''
     
 if __name__ == "__main__":
     app.run('0.0.0.0', 80)
